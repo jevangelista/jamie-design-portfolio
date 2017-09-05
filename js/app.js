c49bbc9309605daf5e0376dadf98571f75bb612a
Jamie Eula Evangelista <j.evangelista88@gmail.com>
2017-09-05 10:40:04 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$('[data-toggle-menu]').on("click", function(){
+  $("#overlay-nav-menu").toggleClass("is-open");
+});
+
+$("[data-menu-underline-from-center] a").addClass("underline-from-center");
+
+$(function () {
+	$('.tlt').textillate()
+})
