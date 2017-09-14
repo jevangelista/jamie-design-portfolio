0d95e7d1175bd02b37f98e197cc5604569abfaaa
Jamie Eula Evangelista <j.evangelista88@gmail.com>
2017-09-14 11:29:03 -0700

    got a functioning up arrow interaction working

--- js/app.js
+++ js/app.js
@@ -9,3 +9,17 @@ $(function () {
 	$('.tlt').textillate()
 })
 
+// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 1000) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 700                     // Scroll to top of body
+    }, 500);
+});
+
