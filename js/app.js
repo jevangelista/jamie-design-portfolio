6a45758eb4b96cb1c6a867d2d7da161475178e0d
Jamie Eula Evangelista <j.evangelista88@gmail.com>
2017-09-15 11:09:22 -0700

    refined back to top button

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ $(function () {
 // ===== Scroll to Top ==== 
 $(window).scroll(function() {
     if ($(this).scrollTop() >= 1000) {        // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+        $('#return-to-top').fadeIn(500);    // Fade in the arrow
     } else {
         $('#return-to-top').fadeOut(200);   // Else fade out the arrow
     }
