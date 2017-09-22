06338d0430b6e4b26cec3cfbf6587cf76b21c63b
Jamie Eula Evangelista <j.evangelista88@gmail.com>
2017-09-22 16:05:23 -0700

    adjusted javascript overlay code and updated instashop process id

--- js/app.js
+++ js/app.js
@@ -1,7 +1,4 @@
 
-$('[data-toggle-menu]').on("click", function(){
-  $("#overlay-nav-menu").toggleClass("is-open");
-});
 
 $("[data-menu-underline-from-center] a").addClass("underline-from-center");
 
@@ -21,6 +18,11 @@ $('#return-to-top').click(function() {      // When arrow is clicked
     $('body,html').animate({
         scrollTop : $("#process").offset().top                  // Scroll to top of body
     }, 500);
+});
+
+
+$('[data-toggle-menu]').on("click", function(){
+  $("#overlay-nav-menu").toggleClass("is-open");
 });
 
 
